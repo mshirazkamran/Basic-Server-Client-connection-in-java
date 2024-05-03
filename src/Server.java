719b933379fa7d89bc515180192ab483c2048a0f
bhaiprogrammer <blogger31625@gmail.com>
2024-05-03 16:00:21 +0500

    addded comments

--- src/Server.java
+++ src/Server.java
@@ -7,9 +7,15 @@ public class Server {
     public static void main(String[] args) throws IOException {
 
         try {
+
             System.out.println("Waiting for connection...");
+
             ServerSocket serverSocket = new ServerSocket(8888);
+            // the control flow of program will stop on this
+            // serverSocket.accept() method and not proceed until
+            // a clients connects to this IP address on the specified port
             Socket server = serverSocket.accept();
+
             System.out.println("Connection established");
 
         } catch (IOException e) {
