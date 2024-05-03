a62e83e5d1aeba06b8bba814c4ebca0fc093c1f4
bhaiprogrammer <blogger31625@gmail.com>
2024-05-03 22:51:41 +0500

    changes

--- src/Client.java
+++ src/Client.java
@@ -8,7 +8,24 @@ public class Client {
 
             System.out.println("Client started!");
             // 127.0.0.1 is localhost
-            Socket client = new Socket("127.0.0.1", 8888);
+            Socket socket = new Socket("127.0.0.1", 14442);
+
+            // System.in gives data in bytes, InputStreamReader wraps that data in
+            // characters and BufferedReader helps in reading that data as a whole line
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            // Prompting user for input
+            System.out.println("Enter some data: ");
+            String data = reader.readLine();
+
+            // Making an output stream that writes data in Characters as opposed to
+            // PrintStream which writes data in bytes
+            PrintWriter dataFromClient = new PrintWriter(socket.getOutputStream(), true);
+            dataFromClient.println(data);
+
+            //Echo effect
+            BufferedReader dataFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println(dataFromServer.readLine());
 
         } catch (IOException e) {
 
